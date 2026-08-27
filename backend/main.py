0a1e594b71fa1deca53e4e36483c780f29de992f
Jose Garcia-Esparza <misc.jose@gmail.com>
2026-08-26 20:25:34 -0500

    Updated fastapi container

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,34 @@
+import os
+
 from fastapi import FastAPI
 import psycopg2
+
+from dotenv import load_dotenv
+load_dotenv()
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
+@app.get("/customers")
+async def get_customers():
+    conn, cur = None, None
+    try:
+        conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+        cur = conn.cursor()
 
-    conn = psycopg2.connect("dbname=ecommerce_db user=admin password=password")
-    cur = conn.cursor()
+        cur.execute("select * from users")
+        users = cur.fetchall()
 
-    cur.execute("select * from users")
-    user = cur.fetchone()
-
-    cur.close()
-    conn.close()
+        cur.close()
+        conn.close()
+        
+        return {"users": users}
     
-    return {"user": user}
+    except psycopg2.Error as e:
+        return {"Error": str(e)}
+
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
