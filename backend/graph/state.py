6ab59328c8cf6973c639aeb6581285594fa36569
Jose Garcia-Esparza <misc.jose@gmail.com>
2026-08-30 14:45:07 -0500

    Created state architecture

---
+++ backend/graph/state.py
@@ -0,0 +1,34 @@
+from datetime import date
+from decimal import Decimal
+
+from typing import TypedDict, Annotated
+from langchain.messages import AnyMessage
+from langgraph.graph.message import add_messages
+
+class ReturnState(TypedDict):
+   order_id: int
+   order_id_attempts: int
+   return_reason: str
+   order_date: date
+   vip_status: bool
+   return_items_total: Decimal
+   latest_return_date: date
+   reason_category: str
+   status: str
+   escalation_reason: str 
+   messages: Annotated[list[AnyMessage], add_messages]
+
+default_return_state = {
+   "order_id": None,
+   "order_id_attempts": 0,
+   "return_reason": None,
+   "order_date": None,
+   "vip_status": False,
+   "return_items_total": Decimal('0.0'),
+   "latest_return_date": None,
+   "reason_category": None,
+   "status": "pending",
+   "escalation_reason": None,
+   "messages": []
+}
+
